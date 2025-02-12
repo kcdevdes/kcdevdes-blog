4818a3934deb23f5499287735eb405b3b31712e9
Gibeom Choi <kcdevdes@gmail.com>
2025-02-12 09:16:12 -0500

    doc: feb-12-2025 post udpate

--- data/categorys.ts
+++ data/categorys.ts
@@ -1,10 +1,10 @@
 export const categorys: { title: string; keyword: string }[] = [
   { title: '🐱 All', keyword: '' },
   { title: '📖 JavaScript', keyword: 'javascript' },
-  { title: '🧑🏻‍💻 회고', keyword: 'retrospection' },
+  { title: '🧑🏻‍💻 Retospection', keyword: 'retrospection' },
   { title: '🖥 Web', keyword: 'web' },
   { title: '👨🏻‍🏫 React', keyword: 'react' },
   { title: '⚙️ Git', keyword: 'git' },
-  { title: '📝 정리', keyword: 'arrangement' },
-  { title: '🎸 기타', keyword: 'other' },
+  { title: '🎸 Other', keyword: 'other' },
+  { title: '📝 Today I Learned...', keyword: 'til' },
 ];
